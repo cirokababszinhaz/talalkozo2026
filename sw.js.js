1b7c4c544bc251ecc67e6641d75dd947a65c2227
cirokababszinhaz <gyorgy.harkai.ciroka@gmail.com>
2026-05-07 11:29:04 +0200

    Add files via upload

--- sw.js.js
+++ sw.js.js
@@ -1,24 +1,34 @@
-const CACHE_NAME = 'babtalalkozo-cache-v1';
-const urlsToCache =[
+const CACHE_NAME = 'babtalalkozo-v2';
+const ASSETS = [
+  './',
   './index.html',
   './manifest.json'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Ha megvan a cache-ben, azt adja vissza (offline mód), egyébként lekéri a netről
-        return response || fetch(event.request);
-      })
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
   );
 });
